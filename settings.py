662662321ba75ce753991e1f2adc2d32e82cde0d
nash kings <51833831+Nash-kings-351@users.noreply.github.com>
2020-01-23 00:06:00 +0300

    Update settings.py
    change bullet width size

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings():
 		# Ship settings
 		self.ship_limit = 3
 		# Bullet settings
-		self.bullet_width = 1000
+		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 60,60,60
 		self.bullets_allowed = 300
